76d6f92b7b858295565df3cc974ef5276ba5598e
bernardosmm <bernardosmm2004@gmail.com>
2024-05-09 16:14:39 -0300

    First commit
    Learning git add and git commit

--- TrainingGit/ContentView.swift
+++ TrainingGit/ContentView.swift
@@ -6,14 +6,13 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           Map()
+                .mapStyle(.standard(elevation: .flat))
         }
         .padding()
     }
