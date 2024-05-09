7edab5d6c0bbeab619eee1f608603b950c51ac01
bernardosmm <bernardosmm2004@gmail.com>
2024-05-09 15:52:52 -0300

    Initial Commit

---
+++ TrainingGit/TrainingGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingGitApp.swift
+//  TrainingGit
+//
+//  Created by Bernardo Santos Maranhão Maia on 09/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TrainingGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
